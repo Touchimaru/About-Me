d88fceb671f49cabb199fb13e34278e1915201b9
Touchimaru <111978620+Touchimaru@users.noreply.github.com>
2023-02-08 03:26:19 -0800

    Add files via upload

---
+++ Hola/script.js
@@ -0,0 +1,16 @@
+function show() {
+ 
+            /* Access image by id and change
+            the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('btnID')
+                    .style.display = "none";
+
+document.querySelector(".Hola").style.display = "none"
+
+document.getElementById("rip").play()  
+
+document.getElementById("rip").style.display = none  
+        } 
